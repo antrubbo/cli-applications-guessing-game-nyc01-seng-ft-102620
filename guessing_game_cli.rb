c201b5d7b154f280cb70aa81e3ce7a3c3768f580
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-10-08 20:24:20 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,11 +1,12 @@
 def run_guessing_game
   num = rand(6) + 1
-  puts "Guess a number between 1 and 6"
   input = gets.chomp
+  puts "Guess a number between 1 and 6"
+  # input = gets.chomp
   if num == input
     puts "You guessed the correct number!"
-  # else
-  #   puts "You guessed the correct number!"
+  else
+    puts "Sorry! The comuter guessed #{num}."
   end
   if input == "exit"
     puts "Goodbye!"
