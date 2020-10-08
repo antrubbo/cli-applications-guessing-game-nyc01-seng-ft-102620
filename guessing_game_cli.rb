ddb7be931281acca2df02bfc83817d2557978ce2
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-10-08 20:40:03 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,9 +7,7 @@ def run_guessing_game
   # end
   if input == "exit"
     puts "Goodbye!"
-  elsif input != num
+  else input != num
     puts "Sorry! The computer guessed #{num}."
-  # else
-  #   puts "You guessed the correct number!"
   end
 end
