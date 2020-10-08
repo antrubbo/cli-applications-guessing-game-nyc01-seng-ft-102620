30b43297a104c4c7638cdf6acce8f92d2853a2b2
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-10-08 20:29:56 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,16 +1,13 @@
-require 'pry'
-
 def run_guessing_game
   num = rand(6) + 1
-  binding.pry
   puts "Guess a number between 1 and 6"
   input = gets.chomp
   
   if input == num
-    puts "You guessed the correct number!"
+    return "You guessed the correct number!"
   elsif input == "exit"
-    puts "Goodbye!"
+    return "Goodbye!"
   else 
-    puts "Sorry! The computer guessed #{num}."
+    return "Sorry! The computer guessed #{num}."
   end
 end
